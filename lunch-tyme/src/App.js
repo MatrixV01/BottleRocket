4f9ca2821681e78696511ff7ab79c6e318352e62
Brad Fincher <matrixv01@gmail.com>
2017-10-07 00:03:36 -0500

    remove web

--- lunch-tyme/src/App.js
+++ lunch-tyme/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Lunch Tyme</h1>
-        </header>
+        </header>        
         <p className="App-intro">
           {/* To get started, edit <code>src/App.js</code> and save to reload. */}
         </p>
